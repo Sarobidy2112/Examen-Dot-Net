b08c8eaf8afd1a19ab2b264345aa78b25825f929
Sarobidy2112 <sarobidyharena21@gmail.com>
2025-05-05 14:17:31 +0200

    Home Page Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using examDotNet.Data;
+using examDotNet.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace examDotNet.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /Home/Index
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            // Vérifier si l'utilisateur est connecté
             if (HttpContext.Session.GetInt32("UserId") == null)
             {
                 return RedirectToAction("Login", "Auth");
             }
 
-            // Récupérer les informations de l'utilisateur depuis la session
             ViewBag.UserName = HttpContext.Session.GetString("UserName");
             ViewBag.UserRole = HttpContext.Session.GetInt32("UserRole");
 
-            return View();
+            var grandCategories = _context.GrandCategories.ToList();
+            var sousCategories = _context.SousCategories
+                                        .Include(sc => sc.Produits)
+                                        .ToList();
+
+            ViewBag.GrandCategories = grandCategories;
+            return View(sousCategories); // modèle principal = sous-catégories
         }
     }
 }
